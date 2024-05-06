4486f913a079702bb25f8fed891e09bb0fca8fb2
EmacsIsBetterThanVI <rdwisema@gmail.com>
2024-05-06 12:56:36 -0400

    added security fixes

--- install.sh
+++ install.sh
@@ -8,18 +8,28 @@ alias refresh=\"sourceall ~/.scripts/*\"
 refresh
 alias nuke=\"rm -rf\"
 REFRESH=true" >> ~/.bash_profile
+echo "function sourceall(){
+for i in \$@; do
+    source \$i
+done
+}
+alias refresh=\"sourceall ~/.scripts/*\"
+refresh
+alias nuke=\"rm -rf\"
+REFRESH=true" >> ~/.bash_alias
 function sourceall(){
 for i in $@; do
     source $i
 done
 }
 alias refresh="sourceall ~/.scripts/*"
-refresh
 alias nuke="rm -rf"
 REFRESH=true
 fi
 [[ -d ~/.config ]] || mkdir ~/.config
 [[ -d ~/.scripts ]] || mkdir ~/.scripts
 mv generate.sh ~/.scripts
-refresh
+for i in $(ls ~/.scripts/*); do
+    source $i
+done
 generate.config
