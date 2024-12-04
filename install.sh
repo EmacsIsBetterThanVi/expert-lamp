a757db148e02a6fc94700c84b275ccb6e44315f4
EmacsIsBetterThanVI <rdwisema@gmail.com>
2024-12-04 10:18:38 -0500

    Removed extra files

--- install.sh
+++ install.sh
@@ -7,22 +7,17 @@ done
 alias refresh=\"sourceall ~/.scripts/*\"
 refresh
 alias nuke=\"rm -rf\"
-REFRESH=true" >> ~/.bash_profile
+export REFRESH=true" >> ~/.bash_profile
 function sourceall(){
 for i in $@; do
     source $i
 done
 }
-alias refresh="sourceall ~/.scripts/*"
+alias refresh="sourceall ~/.scripts/*.sh"
 refresh
 alias nuke="rm -rf"
 REFRESH=true
 fi
 [[ -d ~/.config ]] || mkdir ~/.config
 [[ -d ~/.scripts ]] || mkdir ~/.scripts
-mv GITPKG.sh ~/.scripts
-refresh
-touch ~/.config/expert-lamp.srclist
-touch ~/.config/expert-lampI.pkglist
-touch ~/.config/expert-lampL.pkglist
-touch ~/.config/expert-lamp.pkglist
+mv Noise.py ~
